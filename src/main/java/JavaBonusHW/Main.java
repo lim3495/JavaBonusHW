840eee34e1ca1743df1d237a438dbe8cdb665539
lim3495 <https://github.com/lim3495>
2019-06-19 15:29:13 +0900

    init File

---
+++ src/main/java/JavaBonusHW/Main.java
@@ -0,0 +1,13 @@
+package JavaBonusHW;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BonusDemo myBonus = new BonusDemo();
+		myBonus.run(args);
+
+	}
+
+}
